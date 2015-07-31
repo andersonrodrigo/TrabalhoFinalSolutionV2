30ec3477f1649f92d9738b83a15133eb5f5ace68
luckylethar <fabricio@fabricioweb.com.br>
2015-07-31 09:06:42 -0300

    Implementação do KGB
    KGB implementado. Falta testar.

--- KGB/KgbService.cs
+++ KGB/KgbService.cs
@@ -7,22 +7,52 @@ using ModeloCanonico;
 namespace KGB
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
-    public class KgbService : IKgbService
+    public class KgbService : MarshalByRefObject, ModeloCanonico.Custumer
     {
 
-        public Custumer ObterStatusFinanceiroCliente(string cpf)
+        string nome;
+        float conta;
+        float saldo;
+        public string getNome()
         {
-            throw new NotImplementedException();
+            return nome;
         }
 
-        public Custumer GetCustomerByCPF(string cpf)
+        public Boolean obterStatusFinanceiroCliente(float valorASerTestado)
         {
-            throw new NotImplementedException();
+            if (valorASerTestado <= saldo)
+                return true;
+            else
+                return false;
+        }
+        public float obterDadosConta()
+        {
+            return saldo;
         }
 
-        public Acount ObterDadosConta(Custumer customer)
+        public void setNome(string nome)
         {
-            throw new NotImplementedException();
+            this.nome = nome;
+        }
+
+        public float getConta()
+        {
+            return conta;
+        }
+
+        public void setConta(float conta)
+        {
+            this.conta = conta;
+        }
+
+        public float getSaldo()
+        {
+            return saldo;
+        }
+
+        public void setSaldo(float saldo)
+        {
+            this.saldo = saldo;
         }
     }
 }
